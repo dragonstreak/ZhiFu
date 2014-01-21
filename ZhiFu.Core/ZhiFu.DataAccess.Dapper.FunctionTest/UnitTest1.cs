f85e602bc0ad311f57c82c87b5a9436a64f0a5f1
dragonstreak <dragonstreak_1@163.com>
2014-01-21 16:13:05 +0800

    Add DataAccess.dapper

---
+++ ZhiFu.Core/ZhiFu.DataAccess.Dapper.FunctionTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ZhiFu.DataAccess.Dapper.FunctionTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
